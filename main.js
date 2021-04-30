375a2e49e49d9bab342d7016cb3b9e73107562a0
Piotr Moryto <piotrmoryto@gmail.com>
2021-04-30 17:15:54 +0200

    add calculating display values

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 // update values in bill, tip and split section
 function update() {
-    let bill = document.getElementById("yourBill").value;
+    let bill = Number(document.getElementById("yourBill").value);
     let tipPercent = document.getElementById("tipInput").value;
     let split = document.getElementById("splitInput").value;
-    console.log({
-        bill,
-        tipPercent,
-        split
-    });
+
+    let tipValue = bill * (tipPercent / 100);
+    let tipEach = tipValue / split;
+    let newBillEach = (bill + tipValue) / split;
+
+    document.getElementById("tipPercent").innerHTML = tipPercent + "%";
+    document.getElementById("tipValue").innerHTML = tipValue;
+    document.getElementById("totalWithTip").innerHTML = bill + tipValue;
+    document.getElementById("splitValue").innerHTML = split
+    document.getElementById("billEach").innerHTML = newBillEach;
+    document.getElementById("tipEach").innerHTML = tipEach;
 }
 
 let container = document.getElementById("container");
